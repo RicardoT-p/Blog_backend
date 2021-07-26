590aac3df3af3f4eeffbd9dc25f1d3b0c8d6fcc5
ptang <1654622720@qq.com>
2021-07-27 00:49:04 +0800

    blog-backend1-1 update static and templates

--- src/main/java/com/tpp/web/IndexController.java
+++ src/main/java/com/tpp/web/IndexController.java
@@ -1,9 +1,8 @@
 package com.tpp.web;
 
-import com.tpp.NotFoundException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @ProjectName: blog_backend
@@ -16,13 +15,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String getIndex(@PathVariable Integer id,@PathVariable String name){
-//        String blog = null;
-//        if (blog == null) {
-//            throw new NotFoundException("博客不存在");
-//        }
+    @GetMapping("/")
+    public String getIndex(){
         System.out.println("------index------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        System.out.println("------index------");
+        return "blog";
     }
 }
